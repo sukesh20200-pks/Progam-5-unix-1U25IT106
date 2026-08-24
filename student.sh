aaf0535bdfcb0764c7cdd8156fab2d985667aa19
SUKESH.P <sukesh20200@gmail.com>
2026-08-24 19:35:34 +0530

    Progam 5 unix
    Removed comments and simplified the script to display a message and create a file with content.

--- student.sh
+++ student.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
+echo "Hello,World!"
 
-# Linux Practical: Create a File Using echo
+echo "This is a sample file." > sample.txt
+cat sample.txt
 
-# Aim:
-# To create a file and add content using the echo command.
-
-# Student Task:
-#
-# 1. Display:
-#    Hello, World!
-#
-# 2. Create sample.txt containing:
-#    This is a sample file.
-#
-# 3. Append the following text to sample.txt:
-#    This text will be appended.
-
-# Write your commands below this line:
-
-
+echo "This text will be appended." >> sample.txt
+cat sample.txt
